2899eae2a933f3957b5383dcc36630cb29171259
Pankaj Nikam <pankaj-nikam@users.noreply.github.com>
2020-12-19 19:43:46 +0530

    Optimized - build only calls the favorite icon.

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -6,30 +6,34 @@ import 'package:state_mgmt/screens/product_details_screen.dart';
 class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final _product = Provider.of<Product>(context);
+    final product = Provider.of<Product>(context, listen: false);
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GestureDetector(
         onTap: () {
           Navigator.of(context).pushNamed(
             ProductDetailsScreen.routeName,
-            arguments: _product.id,
+            arguments: product.id,
           );
         },
         child: GridTile(
           child: Image.network(
-            _product.imageUrl,
+            product.imageUrl,
             fit: BoxFit.cover,
           ),
           footer: GridTileBar(
-            leading: IconButton(
-              icon: Icon(
-                _product.isFavorite
-                    ? Icons.favorite
-                    : Icons.favorite_border_outlined,
-              ),
-              onPressed: _product.toggleFavorite,
-              color: Theme.of(context).accentColor,
+            leading: Consumer<Product>(
+              builder: (context, product, _) {
+                return IconButton(
+                  icon: Icon(
+                    product.isFavorite
+                        ? Icons.favorite
+                        : Icons.favorite_border_outlined,
+                  ),
+                  onPressed: product.toggleFavorite,
+                  color: Theme.of(context).accentColor,
+                );
+              },
             ),
             trailing: IconButton(
               icon: Icon(Icons.shopping_cart),
@@ -38,7 +42,7 @@ class ProductItem extends StatelessWidget {
             ),
             backgroundColor: Colors.black87,
             title: Text(
-              _product.title,
+              product.title,
               textAlign: TextAlign.center,
             ),
           ),
