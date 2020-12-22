c66594b27ac7c60b806d30e05558d5cb6430ba94
Pankaj Nikam <pankaj-nikam@users.noreply.github.com>
2020-12-22 20:51:01 +0530

    Added order details.

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import '../providers/orders.dart' as oi;
+import 'dart:math';
 
-class OrderItem extends StatelessWidget {
+class OrderItem extends StatefulWidget {
   final oi.OrderItem _orderItem;
 
   const OrderItem(this._orderItem);
 
+  @override
+  _OrderItemState createState() => _OrderItemState();
+}
+
+class _OrderItemState extends State<OrderItem> {
+  bool isExpandClicked = false;
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -14,16 +21,52 @@ class OrderItem extends StatelessWidget {
       child: Column(
         children: [
           ListTile(
-            title: Text(
-                NumberFormat.compactSimpleCurrency().format(_orderItem.amount)),
+            title: Text(NumberFormat.compactSimpleCurrency()
+                .format(widget._orderItem.amount)),
             subtitle: Text(
-              DateFormat('dd-MM-yyyy HH:mm').format(_orderItem.dateTime),
+              DateFormat('dd-MM-yyyy HH:mm').format(widget._orderItem.dateTime),
             ),
             trailing: IconButton(
-              icon: Icon(Icons.expand_more),
-              onPressed: () {},
+              icon: Icon(isExpandClicked != true
+                  ? Icons.expand_more
+                  : Icons.expand_less),
+              onPressed: () {
+                setState(() {
+                  isExpandClicked = !isExpandClicked;
+                });
+              },
             ),
           ),
+          if (isExpandClicked)
+            Container(
+              height: min(widget._orderItem.products.length * 20.0 + 50.0, 100),
+              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+              child: ListView.builder(
+                itemCount: widget._orderItem.products.length,
+                itemBuilder: (context, index) {
+                  final currentProduct = widget._orderItem.products[index];
+                  return Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        currentProduct.title,
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        '${currentProduct.quantity} x ${NumberFormat.compactSimpleCurrency().format(currentProduct.price)}',
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  );
+                },
+              ),
+            )
         ],
       ),
     );
