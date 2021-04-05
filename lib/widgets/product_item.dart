ba5902aa2de794e225dee51db75acfb35027ad2a
Pankaj Nikam <pankaj-nikam@users.noreply.github.com>
2021-04-05 10:16:00 +0530

    Added placeholder image. Added FadeInImage for placeholder.

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -21,8 +21,11 @@ class ProductItem extends StatelessWidget {
           );
         },
         child: GridTile(
-          child: Image.network(
-            product.imageUrl,
+          child: FadeInImage(
+            placeholder: AssetImage('assets/images/product-placeholder.png'),
+            image: NetworkImage(
+              product.imageUrl,
+            ),
             fit: BoxFit.cover,
           ),
           footer: GridTileBar(
